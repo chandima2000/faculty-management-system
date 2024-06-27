fc08e2e6a7ce8177546747bc6a46e148dd73b4fd
chandima2000 <hychandima2000@gmail.com>
2024-06-27 12:54:32 +0530

    Add Database connection Functions to the Login

--- Faculty-Management-System/Login.cs
+++ Faculty-Management-System/Login.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using String = System.String;
+
 
 namespace Faculty_Management_System
 {
@@ -16,6 +20,8 @@ namespace Faculty_Management_System
         {
             InitializeComponent();
         }
+        
+        SqlConnection conn = new SqlConnection(@"Data Source=.;Initial Catalog=FCT_DB;Integrated Security=True;Encrypt=True;");
 
         private void exit_login_form_Click(object sender, EventArgs e)
         {
@@ -24,9 +30,54 @@ namespace Faculty_Management_System
 
         private void login_btn_Click(object sender, EventArgs e)
         {
-            Home home = new Home();
-            home.Show();
-            this.Hide();
+            String username, password;
+            
+
+            try
+            {
+                conn.Open();
+                String query = "SELECT * FROM Login WHERE username = '"+textBox1.Text+"' AND password = '"+textBox2.Text+"'";
+                SqlDataAdapter sda = new(query,conn);
+
+                DataTable dtable = new();
+                sda.Fill(dtable);
+
+                if(dtable.Rows.Count > 0 )
+                {
+                    username = textBox1.Text;
+                    password = textBox2.Text;
+
+                    Home home = new Home();
+                    home.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox1.Clear();
+                    textBox2.Clear();
+
+                    textBox1.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Something went Wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
