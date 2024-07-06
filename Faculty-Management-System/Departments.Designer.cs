2f45672ef2ce40fdc0c94eff39381ada4433b12c
PiyumiNadeera <dlpnadeera1234@gmail.com>
2024-07-06 18:00:49 +0530

    Database Connection establishment

--- Faculty-Management-System/Departments.Designer.cs
+++ Faculty-Management-System/Departments.Designer.cs
@@ -43,11 +43,11 @@
             label3 = new Label();
             panel2 = new Panel();
             label17 = new Label();
-            label16 = new Label();
-            label15 = new Label();
-            label14 = new Label();
-            label13 = new Label();
-            label12 = new Label();
+            stuCountLabel = new Label();
+            levelLabel = new Label();
+            acYearLabel = new Label();
+            degreeLabel = new Label();
+            deptLabel = new Label();
             label8 = new Label();
             label9 = new Label();
             label10 = new Label();
@@ -62,9 +62,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(528, 44);
+            label1.Location = new Point(603, 59);
             label1.Name = "label1";
-            label1.Size = new Size(293, 24);
+            label1.Size = new Size(366, 29);
             label1.TabIndex = 0;
             label1.Text = "FCT MANAGEMENT SYSTEM";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -74,9 +74,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(620, 85);
+            label2.Location = new Point(709, 113);
             label2.Name = "label2";
-            label2.Size = new Size(140, 20);
+            label2.Size = new Size(174, 25);
             label2.TabIndex = 1;
             label2.Text = "DEPARTMENTS";
             label2.TextAlign = ContentAlignment.MiddleCenter;
@@ -93,32 +93,30 @@
             panel1.Controls.Add(label6);
             panel1.Controls.Add(label5);
             panel1.Controls.Add(label3);
-            panel1.Location = new Point(120, 158);
-            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Location = new Point(137, 211);
             panel1.Name = "panel1";
-            panel1.Size = new Size(570, 320);
+            panel1.Size = new Size(651, 427);
             panel1.TabIndex = 2;
             // 
             // button1
             // 
             button1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(219, 242);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(250, 323);
             button1.Name = "button1";
-            button1.Size = new Size(82, 22);
+            button1.Size = new Size(94, 29);
             button1.TabIndex = 10;
             button1.Text = "SEARCH";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += search_button_click;
             // 
             // comboBox4
             // 
             comboBox4.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             comboBox4.FormattingEnabled = true;
             comboBox4.Items.AddRange(new object[] { "Level I", "Level II", "Level III", "Level IV" });
-            comboBox4.Location = new Point(219, 176);
-            comboBox4.Margin = new Padding(3, 2, 3, 2);
+            comboBox4.Location = new Point(250, 235);
             comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(332, 25);
+            comboBox4.Size = new Size(379, 28);
             comboBox4.TabIndex = 8;
             // 
             // comboBox3
@@ -126,10 +124,9 @@
             comboBox3.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             comboBox3.FormattingEnabled = true;
             comboBox3.Items.AddRange(new object[] { "2018/2019", "2019/2020", "2020/2021", "2021/2022" });
-            comboBox3.Location = new Point(219, 129);
-            comboBox3.Margin = new Padding(3, 2, 3, 2);
+            comboBox3.Location = new Point(250, 172);
             comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(332, 25);
+            comboBox3.Size = new Size(379, 28);
             comboBox3.TabIndex = 7;
             // 
             // comboBox2
@@ -137,10 +134,9 @@
             comboBox2.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "Computer Science", "Computing & Technology", "Engineering Technology" });
-            comboBox2.Location = new Point(219, 82);
-            comboBox2.Margin = new Padding(3, 2, 3, 2);
+            comboBox2.Location = new Point(250, 109);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(332, 25);
+            comboBox2.Size = new Size(379, 28);
             comboBox2.TabIndex = 6;
             // 
             // comboBox1
@@ -148,19 +144,18 @@
             comboBox1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Computer System Engineering", "Applied Computing", "Software Engineering" });
-            comboBox1.Location = new Point(219, 33);
-            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Location = new Point(250, 44);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(332, 25);
+            comboBox1.Size = new Size(379, 28);
             comboBox1.TabIndex = 5;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(32, 82);
+            label7.Location = new Point(37, 109);
             label7.Name = "label7";
-            label7.Size = new Size(68, 20);
+            label7.Size = new Size(82, 25);
             label7.TabIndex = 4;
             label7.Text = "Degree";
             // 
@@ -168,9 +163,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(32, 175);
+            label6.Location = new Point(37, 233);
             label6.Name = "label6";
-            label6.Size = new Size(51, 20);
+            label6.Size = new Size(64, 25);
             label6.TabIndex = 3;
             label6.Text = "Level";
             // 
@@ -178,9 +173,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(32, 128);
+            label5.Location = new Point(37, 171);
             label5.Name = "label5";
-            label5.Size = new Size(130, 20);
+            label5.Size = new Size(158, 25);
             label5.TabIndex = 2;
             label5.Text = "Academic Year";
             // 
@@ -188,9 +183,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(32, 33);
+            label3.Location = new Point(37, 44);
             label3.Name = "label3";
-            label3.Size = new Size(155, 20);
+            label3.Size = new Size(185, 25);
             label3.TabIndex = 0;
             label3.Text = "Department Name";
             label3.Click += label3_Click;
@@ -199,104 +194,103 @@
             // 
             panel2.BackColor = SystemColors.ActiveCaption;
             panel2.Controls.Add(label17);
-            panel2.Controls.Add(label16);
-            panel2.Controls.Add(label15);
-            panel2.Controls.Add(label14);
-            panel2.Controls.Add(label13);
-            panel2.Controls.Add(label12);
+            panel2.Controls.Add(stuCountLabel);
+            panel2.Controls.Add(levelLabel);
+            panel2.Controls.Add(acYearLabel);
+            panel2.Controls.Add(degreeLabel);
+            panel2.Controls.Add(deptLabel);
             panel2.Controls.Add(label8);
             panel2.Controls.Add(label9);
             panel2.Controls.Add(label10);
             panel2.Controls.Add(label11);
             panel2.Controls.Add(label4);
-            panel2.Location = new Point(886, 158);
-            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Location = new Point(1013, 211);
             panel2.Name = "panel2";
-            panel2.Size = new Size(554, 320);
+            panel2.Size = new Size(633, 427);
             panel2.TabIndex = 3;
             // 
             // label17
             // 
             label17.AutoSize = true;
             label17.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label17.Location = new Point(186, 22);
+            label17.Location = new Point(213, 29);
             label17.Name = "label17";
-            label17.Size = new Size(201, 20);
+            label17.Size = new Size(236, 25);
             label17.TabIndex = 14;
             label17.Text = "Department Information";
             // 
-            // label16
+            // stuCountLabel
             // 
-            label16.AutoSize = true;
-            label16.BackColor = SystemColors.Control;
-            label16.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label16.Location = new Point(214, 260);
-            label16.MaximumSize = new Size(88, 8);
-            label16.MinimumSize = new Size(306, 22);
-            label16.Name = "label16";
-            label16.Size = new Size(306, 22);
-            label16.TabIndex = 13;
-            label16.Text = "   ";
+            stuCountLabel.AutoSize = true;
+            stuCountLabel.BackColor = SystemColors.Control;
+            stuCountLabel.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            stuCountLabel.Location = new Point(245, 347);
+            stuCountLabel.MaximumSize = new Size(101, 11);
+            stuCountLabel.MinimumSize = new Size(350, 29);
+            stuCountLabel.Name = "stuCountLabel";
+            stuCountLabel.Size = new Size(350, 29);
+            stuCountLabel.TabIndex = 13;
+            stuCountLabel.Text = "   ";
             // 
-            // label15
+            // levelLabel
             // 
-            label15.AutoSize = true;
-            label15.BackColor = SystemColors.Control;
-            label15.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label15.Location = new Point(214, 208);
-            label15.MaximumSize = new Size(88, 8);
-            label15.MinimumSize = new Size(306, 22);
-            label15.Name = "label15";
-            label15.Size = new Size(306, 22);
-            label15.TabIndex = 12;
-            label15.Text = "   ";
+            levelLabel.AutoSize = true;
+            levelLabel.BackColor = SystemColors.Control;
+            levelLabel.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            levelLabel.Location = new Point(245, 277);
+            levelLabel.MaximumSize = new Size(101, 11);
+            levelLabel.MinimumSize = new Size(350, 29);
+            levelLabel.Name = "levelLabel";
+            levelLabel.Size = new Size(350, 29);
+            levelLabel.TabIndex = 12;
+            levelLabel.Text = "   ";
             // 
-            // label14
+            // acYearLabel
             // 
-            label14.AutoSize = true;
-            label14.BackColor = SystemColors.Control;
-            label14.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label14.Location = new Point(214, 169);
-            label14.MaximumSize = new Size(88, 8);
-            label14.MinimumSize = new Size(306, 22);
-            label14.Name = "label14";
-            label14.Size = new Size(306, 22);
-            label14.TabIndex = 11;
-            label14.Text = "   ";
+            acYearLabel.AutoSize = true;
+            acYearLabel.BackColor = SystemColors.Control;
+            acYearLabel.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            acYearLabel.Location = new Point(245, 225);
+            acYearLabel.MaximumSize = new Size(101, 11);
+            acYearLabel.MinimumSize = new Size(350, 29);
+            acYearLabel.Name = "acYearLabel";
+            acYearLabel.Size = new Size(350, 29);
+            acYearLabel.TabIndex = 11;
+            acYearLabel.Text = "   ";
             // 
-            // label13
+            // degreeLabel
             // 
-            label13.AutoSize = true;
-            label13.BackColor = SystemColors.Control;
-            label13.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label13.Location = new Point(214, 119);
-            label13.MaximumSize = new Size(88, 8);
-            label13.MinimumSize = new Size(306, 22);
-            label13.Name = "label13";
-            label13.Size = new Size(306, 22);
-            label13.TabIndex = 10;
-            label13.Text = "   ";
+            degreeLabel.AutoSize = true;
+            degreeLabel.BackColor = SystemColors.Control;
+            degreeLabel.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            degreeLabel.Location = new Point(245, 159);
+            degreeLabel.MaximumSize = new Size(101, 11);
+            degreeLabel.MinimumSize = new Size(350, 29);
+            degreeLabel.Name = "degreeLabel";
+            degreeLabel.Size = new Size(350, 29);
+            degreeLabel.TabIndex = 10;
+            degreeLabel.Text = "   ";
             // 
-            // label12
+            // deptLabel
             // 
-            label12.AutoSize = true;
-            label12.BackColor = SystemColors.Control;
-            label12.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label12.Location = new Point(214, 73);
-            label12.MaximumSize = new Size(88, 8);
-            label12.MinimumSize = new Size(306, 22);
-            label12.Name = "label12";
-            label12.Size = new Size(306, 22);
-            label12.TabIndex = 9;
-            label12.Text = "   ";
+            deptLabel.AutoSize = true;
+            deptLabel.BackColor = SystemColors.Control;
+            deptLabel.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            deptLabel.Location = new Point(245, 97);
+            deptLabel.MaximumSize = new Size(101, 11);
+            deptLabel.MinimumSize = new Size(350, 29);
+            deptLabel.Name = "deptLabel";
+            deptLabel.Size = new Size(350, 29);
+            deptLabel.TabIndex = 9;
+            deptLabel.Text = "   ";
             // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(34, 122);
+            label8.Location = new Point(39, 163);
             label8.Name = "label8";
-            label8.Size = new Size(68, 20);
+            label8.Size = new Size(82, 25);
             label8.TabIndex = 8;
             label8.Text = "Degree";
             // 
@@ -304,9 +298,9 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label9.Location = new Point(34, 214);
+            label9.Location = new Point(39, 285);
             label9.Name = "label9";
-            label9.Size = new Size(51, 20);
+            label9.Size = new Size(64, 25);
             label9.TabIndex = 7;
             label9.Text = "Level";
             // 
@@ -314,9 +308,9 @@
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label10.Location = new Point(34, 168);
+            label10.Location = new Point(39, 224);
             label10.Name = "label10";
-            label10.Size = new Size(130, 20);
+            label10.Size = new Size(158, 25);
             label10.TabIndex = 6;
             label10.Text = "Academic Year";
             // 
@@ -324,9 +318,9 @@
             // 
             label11.AutoSize = true;
             label11.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label11.Location = new Point(34, 73);
+            label11.Location = new Point(39, 97);
             label11.Name = "label11";
-            label11.Size = new Size(155, 20);
+            label11.Size = new Size(185, 25);
             label11.TabIndex = 5;
             label11.Text = "Department Name";
             // 
@@ -334,9 +328,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(34, 263);
+            label4.Location = new Point(39, 351);
             label4.Name = "label4";
-            label4.Size = new Size(126, 20);
+            label4.Size = new Size(151, 25);
             label4.TabIndex = 2;
             label4.Text = "Student Count";
             // 
@@ -345,10 +339,9 @@
             homeButton.BackColor = SystemColors.ActiveCaptionText;
             homeButton.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             homeButton.ForeColor = SystemColors.ButtonHighlight;
-            homeButton.Location = new Point(1444, 24);
-            homeButton.Margin = new Padding(3, 2, 3, 2);
+            homeButton.Location = new Point(1650, 32);
             homeButton.Name = "homeButton";
-            homeButton.Size = new Size(82, 22);
+            homeButton.Size = new Size(94, 29);
             homeButton.TabIndex = 4;
             homeButton.Text = "Home";
             homeButton.UseVisualStyleBackColor = false;
@@ -356,17 +349,16 @@
             // 
             // Departments
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1518, 565);
+            ClientSize = new Size(1735, 753);
             Controls.Add(homeButton);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Departments";
             Text = "Departments";
             Load += Departments_Load;
@@ -393,11 +385,11 @@
         private ComboBox comboBox3;
         private ComboBox comboBox2;
         private Panel panel2;
-        private Label label16;
-        private Label label15;
-        private Label label14;
-        private Label label13;
-        private Label label12;
+        private Label stuCountLabel;
+        private Label levelLabel;
+        private Label acYearLabel;
+        private Label degreeLabel;
+        private Label deptLabel;
         private Label label8;
         private Label label9;
         private Label label10;
--- Faculty-Management-System/Departments.cs
+++ Faculty-Management-System/Departments.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,11 +19,13 @@ namespace Faculty_Management_System
             InitializeComponent();
         }
 
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=FCT_DB;Integrated Security=True;Encrypt=False;");
+
         private void Departments_Load(object sender, EventArgs e)
         {
-           Home home = new Home();
-           home.Show();
-           this.Hide();
+            Home home = new Home();
+            home.Show();
+            this.Hide();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -35,6 +38,51 @@ namespace Faculty_Management_System
 
         }
 
-        
+        private void search_button_click(object sender, EventArgs e)
+        {
+            String dept_name = comboBox1.SelectedItem.ToString();
+            String degree = comboBox2.SelectedItem.ToString();
+            String ac_year = comboBox3.SelectedItem.ToString();
+            String level = comboBox4.SelectedItem.ToString();
+            int student_count = 0;
+
+            try
+            {
+                conn.Open();
+                String query = "SELECT * FROM departments WHERE name=@dept_name AND degree=@degree AND year=@ac_year AND level=@level";
+                SqlCommand cmd = new(query, conn);
+                cmd.Parameters.AddWithValue("@dept_name", dept_name);
+                cmd.Parameters.AddWithValue("@degree", degree);
+                cmd.Parameters.AddWithValue("@ac_year", ac_year);
+                cmd.Parameters.AddWithValue("@level", level);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    deptLabel.Text = reader["name"].ToString();
+                    degreeLabel.Text = reader["degree"].ToString();
+                    acYearLabel.Text = reader["year"].ToString();
+                    levelLabel.Text = reader["level"].ToString();
+                    student_count = Convert.ToInt32(reader["student_count"]);
+                    stuCountLabel.Text = student_count.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No records found.");
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
     }
 }
