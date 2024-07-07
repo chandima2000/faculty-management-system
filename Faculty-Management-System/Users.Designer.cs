976a16c415a9c143442f1196eb6ff092d9d4434f
Kaviii8 <kavindiliyanage.350@gmail.com>
2024-07-07 21:46:23 +0530

    Users backend

--- Faculty-Management-System/Users.Designer.cs
+++ Faculty-Management-System/Users.Designer.cs
@@ -31,24 +31,25 @@
             label1 = new Label();
             label2 = new Label();
             panel1 = new Panel();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
-            label9 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            button10 = new Button();
             button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
             panel2 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -75,16 +76,17 @@
             // panel1
             // 
             panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(textBox7);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
             panel1.Controls.Add(button11);
             panel1.Controls.Add(button10);
             panel1.Controls.Add(button9);
             panel1.Controls.Add(button8);
-            panel1.Controls.Add(button6);
-            panel1.Controls.Add(button5);
-            panel1.Controls.Add(button4);
-            panel1.Controls.Add(button3);
-            panel1.Controls.Add(button2);
-            panel1.Controls.Add(button1);
             panel1.Controls.Add(label9);
             panel1.Controls.Add(label8);
             panel1.Controls.Add(label7);
@@ -97,129 +99,27 @@
             panel1.Size = new Size(578, 566);
             panel1.TabIndex = 2;
             // 
-            // label3
+            // button11
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(26, 29);
-            label3.Name = "label3";
-            label3.Size = new Size(60, 20);
-            label3.TabIndex = 0;
-            label3.Text = "User ID:";
+            button11.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.Location = new Point(222, 477);
+            button11.Name = "button11";
+            button11.Size = new Size(125, 29);
+            button11.TabIndex = 17;
+            button11.Text = "Home";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
             // 
-            // label4
+            // button10
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(26, 79);
-            label4.Name = "label4";
-            label4.Size = new Size(52, 20);
-            label4.TabIndex = 1;
-            label4.Text = "Name:";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(26, 134);
-            label5.Name = "label5";
-            label5.Size = new Size(64, 20);
-            label5.TabIndex = 2;
-            label5.Text = "Position:";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(26, 190);
-            label6.Name = "label6";
-            label6.Size = new Size(53, 20);
-            label6.TabIndex = 3;
-            label6.Text = "Phone:";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(26, 246);
-            label7.Name = "label7";
-            label7.Size = new Size(49, 20);
-            label7.TabIndex = 4;
-            label7.Text = "Email:";
-            label7.Click += label7_Click;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(26, 304);
-            label8.Name = "label8";
-            label8.Size = new Size(73, 20);
-            label8.TabIndex = 5;
-            label8.Text = "Password:";
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(26, 364);
-            label9.Name = "label9";
-            label9.Size = new Size(51, 20);
-            label9.TabIndex = 6;
-            label9.Text = "Photo:";
-            label9.Click += label9_Click;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(130, 25);
-            button1.Name = "button1";
-            button1.Size = new Size(427, 29);
-            button1.TabIndex = 7;
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(130, 75);
-            button2.Name = "button2";
-            button2.Size = new Size(427, 29);
-            button2.TabIndex = 8;
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(130, 130);
-            button3.Name = "button3";
-            button3.Size = new Size(427, 29);
-            button3.TabIndex = 9;
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(130, 186);
-            button4.Name = "button4";
-            button4.Size = new Size(427, 29);
-            button4.TabIndex = 10;
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(130, 242);
-            button5.Name = "button5";
-            button5.Size = new Size(427, 29);
-            button5.TabIndex = 11;
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(130, 300);
-            button6.Name = "button6";
-            button6.Size = new Size(427, 29);
-            button6.TabIndex = 12;
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            button8.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button8.Location = new Point(35, 426);
-            button8.Name = "button8";
-            button8.Size = new Size(125, 29);
-            button8.TabIndex = 14;
-            button8.Text = "Add";
-            button8.UseVisualStyleBackColor = true;
-            button8.Click += button8_Click;
+            button10.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.Location = new Point(415, 426);
+            button10.Name = "button10";
+            button10.Size = new Size(125, 29);
+            button10.TabIndex = 16;
+            button10.Text = "Delete";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
             // 
             // button9
             // 
@@ -232,26 +132,81 @@
             button9.UseVisualStyleBackColor = true;
             button9.Click += button9_Click;
             // 
-            // button10
+            // button8
             // 
-            button10.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button10.Location = new Point(415, 426);
-            button10.Name = "button10";
-            button10.Size = new Size(125, 29);
-            button10.TabIndex = 16;
-            button10.Text = "Delete";
-            button10.UseVisualStyleBackColor = true;
+            button8.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(35, 426);
+            button8.Name = "button8";
+            button8.Size = new Size(125, 29);
+            button8.TabIndex = 14;
+            button8.Text = "Add";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
             // 
-            // button11
+            // label9
             // 
-            button11.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button11.Location = new Point(222, 477);
-            button11.Name = "button11";
-            button11.Size = new Size(125, 29);
-            button11.TabIndex = 17;
-            button11.Text = "Home";
-            button11.UseVisualStyleBackColor = true;
-            button11.Click += button11_Click;
+            label9.AutoSize = true;
+            label9.Location = new Point(26, 364);
+            label9.Name = "label9";
+            label9.Size = new Size(51, 20);
+            label9.TabIndex = 6;
+            label9.Text = "Photo:";
+            label9.Click += label9_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(26, 304);
+            label8.Name = "label8";
+            label8.Size = new Size(73, 20);
+            label8.TabIndex = 5;
+            label8.Text = "Password:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(26, 246);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 20);
+            label7.TabIndex = 4;
+            label7.Text = "Email:";
+            label7.Click += label7_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(26, 190);
+            label6.Name = "label6";
+            label6.Size = new Size(53, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Phone:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(26, 134);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Position:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 79);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 20);
+            label3.TabIndex = 0;
+            label3.Text = "User ID:";
             // 
             // panel2
             // 
@@ -260,6 +215,55 @@
             panel2.Name = "panel2";
             panel2.Size = new Size(564, 266);
             panel2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(130, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(427, 27);
+            textBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(130, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(427, 27);
+            textBox2.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(130, 134);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(427, 27);
+            textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(130, 190);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(427, 27);
+            textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(130, 239);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(427, 27);
+            textBox5.TabIndex = 22;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(130, 297);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(427, 27);
+            textBox6.TabIndex = 23;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(130, 357);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(427, 27);
+            textBox7.TabIndex = 24;
             // 
             // Users
             // 
@@ -293,16 +297,17 @@
         private Label label5;
         private Label label4;
         private Label label3;
-        private Button button3;
-        private Button button2;
-        private Button button1;
         private Button button11;
         private Button button10;
         private Button button9;
         private Button button8;
-        private Button button6;
-        private Button button5;
-        private Button button4;
         private Panel panel2;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
     }
 }
--- Faculty-Management-System/Users.cs
+++ Faculty-Management-System/Users.cs
@@ -7,34 +7,164 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Faculty_Management_System
 {
     public partial class Users : Form
     {
+        private string connectionString = "Data Source=DESKTOP-ELFA8OA\\SQLEXPRESS;Initial Catalog=FacultyManagement;Integrated Security=True;TrustServerCertificate=True";
+
         public Users()
         {
             InitializeComponent();
+            InitializeCustomComponents();
+            LoadData();
         }
 
-        private void label7_Click(object sender, EventArgs e)
-        {
+        // Define text boxes
+        private TextBox UserIDTextBox;
+        private TextBox NameTextBox;
+        private TextBox PositionTextBox;
+        private TextBox PhoneTextBox;
+        private TextBox EmailTextBox;
+        private TextBox PasswordTextBox;
+        private TextBox PhotoTextBox;
 
+        // Define the DataGridView for displaying users
+        private DataGridView usersDataGridView;
+
+        private void InitializeCustomComponents()
+        {
+            this.UserIDTextBox = new TextBox() { Location = new Point(100, 20), Size = new Size(200, 20), Name = "UserIDTextBox" };
+            this.NameTextBox = new TextBox() { Location = new Point(100, 60), Size = new Size(200, 20), Name = "NameTextBox" };
+            this.PositionTextBox = new TextBox() { Location = new Point(100, 100), Size = new Size(200, 20), Name = "PositionTextBox" };
+            this.PhoneTextBox = new TextBox() { Location = new Point(100, 140), Size = new Size(200, 20), Name = "PhoneTextBox" };
+            this.EmailTextBox = new TextBox() { Location = new Point(100, 180), Size = new Size(200, 20), Name = "EmailTextBox" };
+            this.PasswordTextBox = new TextBox() { Location = new Point(100, 220), Size = new Size(200, 20), Name = "PasswordTextBox" };
+            this.PhotoTextBox = new TextBox() { Location = new Point(100, 260), Size = new Size(200, 20), Name = "PhotoTextBox" };
+
+            this.usersDataGridView = new DataGridView() { Location = new Point(320, 20), Size = new Size(400, 300), Name = "usersDataGridView" };
+
+            this.Controls.Add(this.UserIDTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.PositionTextBox);
+            this.Controls.Add(this.PhoneTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.PhotoTextBox);
+            this.Controls.Add(this.usersDataGridView);
         }
 
-        private void label9_Click(object sender, EventArgs e)
+        private void LoadData()
         {
+            if (usersDataGridView == null)
+            {
+                MessageBox.Show("DataGridView is not initialized.");
+                return;
+            }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Users";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                usersDataGridView.DataSource = dataTable;
+            }
+        }
 
+        private void AddRecord()
+        {
+            // Check if any required fields are empty
+            if (string.IsNullOrEmpty(NameTextBox.Text) || string.IsNullOrEmpty(PositionTextBox.Text) ||
+                string.IsNullOrEmpty(PhoneTextBox.Text) || string.IsNullOrEmpty(EmailTextBox.Text) ||
+                string.IsNullOrEmpty(PasswordTextBox.Text) || string.IsNullOrEmpty(PhotoTextBox.Text))
+            {
+                MessageBox.Show("Please fill all fields.");
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Users (Name, Position, Phone, Email, Password, Photo) VALUES (@Name, @Position, @Phone, @Email, @Password, @Photo)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", NameTextBox.Text);
+                    command.Parameters.AddWithValue("@Position", PositionTextBox.Text);
+                    command.Parameters.AddWithValue("@Phone", PhoneTextBox.Text);
+                    command.Parameters.AddWithValue("@Email", EmailTextBox.Text);
+                    command.Parameters.AddWithValue("@Password", PasswordTextBox.Text);
+                    command.Parameters.AddWithValue("@Photo", PhotoTextBox.Text);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            LoadData(); // Refresh the data grid view
+        }
+
+        private void EditRecord()
+        {
+            if (string.IsNullOrEmpty(UserIDTextBox.Text))
+            {
+                MessageBox.Show("Please enter a UserID.");
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Users SET Name=@Name, Position=@Position, Phone=@Phone, Email=@Email, Password=@Password, Photo=@Photo WHERE ID=@UserID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserID", UserIDTextBox.Text);
+                    command.Parameters.AddWithValue("@Name", NameTextBox.Text);
+                    command.Parameters.AddWithValue("@Position", PositionTextBox.Text);
+                    command.Parameters.AddWithValue("@Phone", PhoneTextBox.Text);
+                    command.Parameters.AddWithValue("@Email", EmailTextBox.Text);
+                    command.Parameters.AddWithValue("@Password", PasswordTextBox.Text);
+                    command.Parameters.AddWithValue("@Photo", PhotoTextBox.Text);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            LoadData(); // Refresh the data grid view
+        }
+
+        private void DeleteRecord()
+        {
+            if (string.IsNullOrEmpty(UserIDTextBox.Text))
+            {
+                MessageBox.Show("Please enter a UserID.");
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Users WHERE ID=@UserID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserID", UserIDTextBox.Text);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            LoadData(); // Refresh the data grid view
+        }
+
+        private void Users_Load(object sender, EventArgs e)
+        {
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            AddRecord();
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            EditRecord();
         }
 
         private void button11_Click(object sender, EventArgs e)
@@ -44,9 +174,9 @@ namespace Faculty_Management_System
             this.Hide();
         }
 
-        private void Users_Load(object sender, EventArgs e)
+        private void button10_Click(object sender, EventArgs e)
         {
-
+            DeleteRecord();
         }
     }
 }
